e86c284792e2aba870480bf65449b640a2e1802c
kprasad1221 <kprasad.1221@gmail.com>
2020-06-23 15:14:36 -0800

    Bind Keyboard input to VRCharacter movement

--- ArchitectureExplorer/Intermediate/Build/Win64/UE4Editor/Inc/ArchitectureExplorer/ArchitectureExplorer.init.gen.cpp
+++ ArchitectureExplorer/Intermediate/Build/Win64/UE4Editor/Inc/ArchitectureExplorer/ArchitectureExplorer.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeArchitectureExplorer_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x5BF28A37,
-				0xB4D05979,
+				0xE0EF6362,
+				0xF1E4AB5B,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
